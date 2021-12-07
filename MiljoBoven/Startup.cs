37d01988294b15124fbec8289d8ed0812cbe709c
Benjamin Appelberg <benjamin.williamsappelberg.0315@student.uu.se>
2021-12-07 14:51:26 +0100

    Added Microsoft.Extensions.Configuration for Configuration service added connection to database in ConfigureServices

--- MiljoBoven/Startup.cs
+++ MiljoBoven/Startup.cs
@@ -10,14 +10,24 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MiljoBoven.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace MiljoBoven
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        // Constructor
+        public Startup(IConfiguration config) => Configuration = config;
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddTransient<IEnvironmentCrimeRepository, FakeEnvironmentCrimeRepository>();  // Här talar vi om att vi vill arbeta med Interfacet och implementeringen
+        {   
+            //Add connection to database
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IEnvironmentCrimeRepository, EFEnvironmentCrimeRepository>();  // Här talar vi om att vi vill arbeta med Interfacet och implementeringen
+
             services.AddControllersWithViews();
         }
 
