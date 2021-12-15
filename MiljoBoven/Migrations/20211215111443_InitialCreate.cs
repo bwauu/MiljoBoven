d12f7c1c38cb0994250e15b10f8fb98dff9b50ec
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2021-12-15 12:31:00 +0100

    pc stationary push.
    what have I done?
    
    Initialized a database migration following these steps:
    
    1.  Tools => NuGet Packet Manager
                                  => Manage NuGet Packages 4 Solution
    2.  Search1: Microsoft.EntityFrameworkCore.SqlServer and download.
    Search2: Microsoft.EntityFrameworkCore.Tools and download
    ---------------------------------------------------------
    public class Startup
        {
    
            public IConfiguration Configuration { get; }
    
            public Startup(IConfiguration config) => Configuration = config;
            // This method gets called by the runtime. Use this method to add services to the container.
            public void ConfigureServices(IServiceCollection services)
            {
                services.AddDbContext<ApplicationDbContext>(options =>
                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
    
                services.AddTransient<IEnvironmentCrimeRepository, EFEnvironmentCrimeRepository>();  // Här talar vi om att vi vill arbeta med Interfacet och implementeringen
                services.AddControllersWithViews();
                services.AddSession();
    
            }
    
            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
            {
                if (env.IsDevelopment())
                {
                    app.UseDeveloperExceptionPage();
                }
    
                app.UseStatusCodePages();
                app.UseStaticFiles();
    
                app.UseRouting();
    
                app.UseSession();
    
                app.UseEndpoints(endpoints =>
                {
                    endpoints.MapDefaultControllerRoute();
                });
            }
    
        }
    ---------------------------------------------------------
        public class Program
        {
            public static void Main(string[] args)
            {
                var host = CreateHostBuilder(args).Build();
                using (var scope = host.Services.CreateScope())
                {
                    var services = scope.ServiceProvider;
                    try
                    {
                        var context = services.GetRequiredService<ApplicationDbContext>();
                        DBInitializer.EnsurePopulated(context);
                    }
                    catch (Exception ex)
                    {
                        var logger = services.GetRequiredService<ILogger<Program>>();
                        logger.LogError(ex, "Ett fel uppstod när databasen skulle fyllas med data");
                    }
                }
                host.Run();
            }
    
            public static IHostBuilder CreateHostBuilder(string[] args) =>
                Host.CreateDefaultBuilder(args)
                    .ConfigureWebHostDefaults(webBuilder =>
                    {
                        webBuilder.UseStartup<Startup>();
                    });
        }
    ---------------------------------------------------------
    
    3. Open Package Manager Console:

---
+++ MiljoBoven/Migrations/20211215111443_InitialCreate.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiljoBoven.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Samples_ErrandId",
+                table: "Samples",
+                column: "ErrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pictures_ErrandId",
+                table: "Pictures",
+                column: "ErrandId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pictures_Errands_ErrandId",
+                table: "Pictures",
+                column: "ErrandId",
+                principalTable: "Errands",
+                principalColumn: "ErrandId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Samples_Errands_ErrandId",
+                table: "Samples",
+                column: "ErrandId",
+                principalTable: "Errands",
+                principalColumn: "ErrandId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pictures_Errands_ErrandId",
+                table: "Pictures");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Samples_Errands_ErrandId",
+                table: "Samples");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Samples_ErrandId",
+                table: "Samples");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pictures_ErrandId",
+                table: "Pictures");
+        }
+    }
+}
