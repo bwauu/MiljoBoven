69925d4389cfee7eb0b7e261edec8a6c40def81c
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2021-12-19 21:45:02 +0100

    Identity föreläsning klar

--- MiljoBoven/Controllers/AccountController.cs
+++ MiljoBoven/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiljoBoven.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,48 @@ using System.Threading.Tasks;
 
 namespace MiljoBoven.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManagerx, SignInManager<IdentityUser> signInManagerx)
         {
-            return View();
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        [AllowAnonymous] // alla kan kommma åt sidan
+        public ViewResult Login(string returnURL)
+        {
+            return View(new LoginModel { ReturnURL = returnURL});
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken] // Skyddar mot XXS, taghelper
+        public async Task<IActionResult> Login (LoginModel loginModel)
+        {   
+            IdentityUser user = await userManager.FindByNameAsync(loginModel.UserName);
+            
+            if(ModelState.IsValid)
+            {
+                if (user != null) // kontrollera att namnet finns i db
+                {
+                    await signInManager.SignOutAsync(); // Om användaren är redan inlogga så tas sessionen bort
+                    if ((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect("/Account/Manager");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Felaktigt användarnamn eller lösenord");
+            return View(loginModel);
+        }
+
+        public async Task<RedirectResult> Logout(string returnURL = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnURL);
         }
     }
 }
---
+++ MiljoBoven/Models/Poco/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiljoBoven.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Vänligen fyll i användarnamn!")]
+        [Display(Name = "Användarnamn:")] // Ändrar default från eng t sv
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Vänligen fyll i lösenord!")]
+        [Display(Name = "Lösenord:")] // Ändrar default från eng t sv
+        [UIHint("password")] // berättar om vilken typ. kommer inte visa några tecken.
+        public string Password { get; set; }
+        // ReturnURL används genom att skicka runt mellan olika sidor. vart man ska tillbaka. Man använder / för att berätta gå tillbaka där du kom ifrån
+        public string ReturnURL { get; set; }
+    }
+}
